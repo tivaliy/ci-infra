89530f8d050d3535bd05abdbc283cca9f2a778b7
Vitalii Kulanov <vitaliy@kulanov.org.ua>
2017-01-25 06:58:18 +0200

    Add '-f/--force' flag for destroy_ci.sh script
    Adds '-f/--force' flag that allows to remove
    docker data volumes for respective docker containers
    while executing ./destroy_ci.sh script, e.g.:
    
       ./destroy_ci.sh --force

--- destroy_ci.sh
+++ destroy_ci.sh
@@ -6,3 +6,9 @@ source ./env.config
 
 # Stops containers and removes containers, networks and volumes
 docker-compose down
+
+# Remove docker data volumes if '-f/--force' flag specified
+if [[ ( "$1" == "-f" ) || ( "$1" == "--force" ) ]]; then
+    echo "Removing data volumes in "${VOLUME_PATH}
+    sudo rm -rf ${VOLUME_PATH}
+fi
