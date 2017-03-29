d4639446eceb74102b0ba78d50db35fe44dd4954
Vitalii Kulanov <vitaliy@kulanov.org.ua>
2017-03-29 12:06:24 +0300

    Align path to gerritlib git repository

--- tools/gerrit/create-projects.sh
+++ tools/gerrit/create-projects.sh
@@ -10,19 +10,23 @@ export PROJECTS_YAML=projects.yaml
 git clone https://github.com/openstack-infra/jeepyb.git
 
 # Create and activate virtualenv
-virtualenv -p python2.7 jeepyb/.jeepyb_venv
-source jeepyb/.jeepyb_venv/bin/activate
+virtualenv -p python2.7 jeepyb_venv
+source jeepyb_venv/bin/activate
 
 # Install jeepyb with all necessary dependencies
+echo 'setuptools!=34.0.1' >> jeepyb/requirements.txt
+pip install -r jeepyb/requirements.txt
 pip install jeepyb/
+rm -rf jeepyb
 
 # This is temporary part that should be removed after release of gerritlib 0.6.1.
 # Update gerritlib manually to match the command for Gerrit-2.12+.
-git clone https://github.com/openstack-infra/gerritlib
+git clone https://github.com/openstack-infra/gerritlib.git
 pip install gerritlib/
+rm -rf gerritlib
 
 echo "Creating projects in Gerrit"
 manage-projects -v -d
 
 echo "Cleaning..."
-rm -rf gerritlib/ jeepyb/
+rm -rf jeepyb_venv
